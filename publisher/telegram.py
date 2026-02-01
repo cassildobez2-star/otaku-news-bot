05bfc4888a9bb61c317d73584f8243a172c6510a
cassildobez2-star <cassildobez2@gmail.com>
2026-02-01 05:49:45 +0200

    Update telegram.py

--- publisher/telegram.py
+++ publisher/telegram.py
@@ -1,1 +1,16 @@
+import time
+from telegram.error import RetryAfter
+from bot import bot
+from config import CHANNEL_ID
 
+def publish(text, image):
+    try:
+        bot.send_photo(
+            chat_id=CHANNEL_ID,
+            photo=image,
+            caption=text,
+            parse_mode="Markdown"
+        )
+    except RetryAfter as e:
+        time.sleep(e.retry_after)
+        publish(text, image)
