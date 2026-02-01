2feb3549ce62775b1efc12d65deedb160aa471d6
cassildobez2-star <cassildobez2@gmail.com>
2026-02-01 05:45:15 +0200

    Update anilist.py

--- fetcher/anilist.py
+++ fetcher/anilist.py
@@ -1,1 +1,24 @@
+import requests
 
+API_URL = "https://graphql.anilist.co"
+
+QUERY = """
+query ($id: Int) {
+  Media (id: $id, type: ANIME) {
+    id
+    title { romaji }
+    description
+    coverImage { large }
+    startDate { year }
+  }
+}
+"""
+
+def fetch_anime(anime_id):
+    r = requests.post(API_URL, json={
+        "query": QUERY,
+        "variables": {"id": anime_id}
+    })
+    if r.status_code != 200:
+        return None
+    return r.json().get("data", {}).get("Media")
