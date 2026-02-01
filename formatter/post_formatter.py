da56fa4bb560054d1008d3652c080d31e1838495
cassildobez2-star <cassildobez2@gmail.com>
2026-02-01 05:47:23 +0200

    Update post_formatter.py

--- formatter/post_formatter.py
+++ formatter/post_formatter.py
@@ -1,1 +1,14 @@
+def format_anime_post(anime):
+    title = anime["title"]["romaji"]
+    year = anime["startDate"]["year"]
+    desc = anime["description"] or "Sem descrição."
+    desc = desc.replace("<br>", "").replace("</br>", "")[:700]
 
+    text = (
+        f"🎬 *Anime*\n\n"
+        f"📌 *Título:* {title}\n"
+        f"📅 *Ano:* {year}\n\n"
+        f"📖 *Sinopse:*\n{desc}\n\n"
+        f"#anime #otaku"
+    )
+    return text, anime["coverImage"]["large"]
