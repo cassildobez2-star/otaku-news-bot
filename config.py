cbfe7a6d6309378a65531c0f78bb03bf941d4495
cassildobez2-star <cassildobez2@gmail.com>
2026-02-01 05:53:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")  # ex: -100xxxxxxxxxx
+
+MODE = "BACKLOG"  # BACKLOG ou DAILY
+
+INTERVAL_BACKLOG = 20          # segundos
+INTERVAL_DAILY = 45 * 60       # 45 minutos
+MAX_DAILY_POSTS = 12
